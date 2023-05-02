46c13877943fd4a838520d898a0e20daa9b06b92
KarpenkoIgor <hv.karpenko@gmail.com>
2023-05-02 15:59:47 +0300

    Added correct working zoom function

--- frontend/src/map_editor/Canvas.js
+++ frontend/src/map_editor/Canvas.js
@@ -7,9 +7,19 @@ function FabricCanvas() {
   const canvasRef = useRef(null);
   const [isDrawing, setIsDrawing] = useState(false);
   const [isSelecting, setIsSelecting] = useState(false);
+  const [isErasing, setIsErasing] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [isSaving, setIsSaving] = useState(false);
   const [selectedObject, setSelectedObject] = useState(null);
   const [canvasObjects, setCanvasObjects] = useState([]);
   const [isDoubleSided, setIsDoubleSided] = useState(false);
+  const [canvasZoom, setCanvasZoom] = useState(1);
+  const [canvasPosition, setCanvasPosition] = useState({ x: 0, y: 0});
+
+  const setAllFalse = () => {
+    setIsDrawing(false);
+    setIsSelecting(false);
+  }
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -24,17 +34,33 @@ function FabricCanvas() {
       fabricCanvas.add(obj);
     });
 
+    fabricCanvas.setZoom(canvasZoom);
+    fabricCanvas.relativePan({x: canvasPosition.x ,y: canvasPosition.y});
+
     if (isDrawing) {
       handleDrawing(fabricCanvas, canvasObjects, setCanvasObjects, isDoubleSided);
     } else if (isSelecting) {
 
     }
 
+    fabricCanvas.on('mouse:wheel', (opt) => {
+      var delta = opt.e.deltaY;
+      var zoom = fabricCanvas.getZoom();
+      zoom *= 0.999 ** delta;
+      if (zoom > 10) zoom = 10;
+      if (zoom < 0.1) zoom = 0.1;
+      fabricCanvas.zoomToPoint(fabricCanvas.getPointer(),zoom);
+      setCanvasZoom(zoom);
+      setCanvasPosition({ x: fabricCanvas.viewportTransform[4], y: fabricCanvas.viewportTransform[5] });
+      opt.e.preventDefault();
+      opt.e.stopPropagation();
+    });
+
     return () => {
       fabricCanvas.dispose();
     };
 
-  }, [isDrawing, isSelecting, isDoubleSided, canvasObjects]);
+  }, [isDrawing, isSelecting, isDoubleSided, canvasObjects,]);
 
   const handleDrawButtonClick = (event) => {
     event.preventDefault();
